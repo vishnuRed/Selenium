b2f7c51f1017fac4bef89f61610dc9780f7f3d83
vishnuRed <vishnu.virtual@gmail.com>
2014-08-16 09:19:34 +0530

    Update SeleniumExample.java
    added sysout staements

--- selenium example/src/com/pkma/qa/selinium/SeleniumExample.java
+++ selenium example/src/com/pkma/qa/selinium/SeleniumExample.java
@@ -24,26 +24,31 @@ public class SeleniumExample {
 	  private String baseUrl;
 	  @Before
 	  public void setUp() throws Exception {
+	  	System.out.println("Hai i am at start of setup  ");
 	    driver = new FirefoxDriver();
 	    driver.manage().window().maximize();
 	    baseUrl = "https://www.google.co.in/";
 	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    System.out.println("Hai i am  at end of setup ");
 	    
 	  }
 
 	  @Test
 	  public void testSELENIUMeXAMPLE() throws Exception {
+	  	System.out.println("Hai i am at start of test  ");
 	    driver.get(baseUrl);
 	    driver.findElement(By.id("gs_tti0")).click();
 	    driver.findElement(By.id("gbqfq")).clear();
 	    driver.findElement(By.id("gbqfq")).sendKeys("SELENIUM");
 	    driver.findElement(By.id("gbqfb")).click();
+	    System.out.println("Hai i am at end of test  ");
 	  }
 
 	  @After
 	  public void tearDown() throws Exception {
+	  	System.out.println("Hai i am at start of After method ");
 	    driver.quit();
-	    
+	    System.out.println("Hai i am at end of After method ");
 	}
 	
 
